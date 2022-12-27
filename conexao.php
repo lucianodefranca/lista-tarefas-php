dfc3fb00fc5d66ae054d04cf1b4561ad28d4456f
luciano de franca <lucianodefranca@gmail.com>
2022-12-26 21:47:07 -0300

    classe conexao com base de dados

---
+++ conexao.php
@@ -0,0 +1,32 @@
+<?php 
+
+    class Conexao {
+
+        private $host = 'localhost';
+        private $dbname = 'ista_tarefas_com_pdo';
+        private $user = 'root';
+        private $pass = '';
+
+        public function conectar() {
+            try {
+
+                $conexao = new PDO(
+                    "mysql:host=$this->host;dbname=$this->dbname",
+                    "$this->user",
+                    "$this->pass"
+                );
+
+                return $conexao;
+
+
+            } catch (PDOException $e) {
+
+                echo '<p>'.$e->getMessege().'</p>';
+
+            }
+        }
+    }
+
+
+
+?>
